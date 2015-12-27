1dd933e79e62fffa45179b49bdcd1d42c23d3ef9
Iblis Lin <e196819@hotmail.com>
2015-12-28 02:14:20 +0800

    [py3] improve compatibility

--- autoload/simulator.py
+++ autoload/simulator.py
@@ -1,18 +1,21 @@
 #!/bin/env python
 
+from __future__ import print_function
+
 from nimrod_vim import execNimCmd
+
 
 proj = "/foo"
 
-while True:
-  line = raw_input("enter command: ")
-  async = False
-  
-  if line == "quit":
-    async = True
-  
-  print execNimCmd(proj, line, async)
 
-  if line == "quit":
-    break
-  
+while True:
+    line = raw_input("enter command: ")
+    async_ = False
+
+    if line == 'quit':
+        async_ = True
+
+    print(execNimCmd(proj, line, async_))
+
+    if line == 'quit':
+        break
